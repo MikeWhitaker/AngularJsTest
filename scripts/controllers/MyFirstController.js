adb29687eb6408fa401c5c6576eeb3dfd90d0080
DESKTOP-3SE1LP0\Mike <whitaker.mj@gmail.com>
2018-01-21 11:42:10 +0100

    Working: list of items provided from service.

--- scripts/controllers/MyFirstController.js
+++ scripts/controllers/MyFirstController.js
@@ -8,7 +8,16 @@
  * Controller of the childListApp
  */
 angular.module('childListApp')
-    .controller('MyFirstController', function () {
+    .controller('MyFirstController', function (familyservice) {
+
+      var vm = this;
+      var templistFamily = familyservice.query();
+      templistFamily.$promise.then(function (result) {
+          vm.listFamily = result;
+
+      });
+
+      console.log('listFamily: ' + vm.listFamily);
       //this.awesomeThings = [
       //    'HTML5 Boilerplate',
       //    'AngularJS',
